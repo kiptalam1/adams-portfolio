14b8a49a37a3e0718c551719115b817f5ce4ea5b
kiptalam1 <adamskiptalam0@gmail.com>
2026-04-03 22:56:56 +0300

    feat: build contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Contacts from "./components/Contacts";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
 
 export default function App() {
-	return (
-		<div className="h-full min-h-screen w-full min-w-screen bg-bg text-text text-base font-urbanist p-4 sm:p-6 md:p-8 lg:p-10 space-y-4">
-			<Header />
-			<Technologies />
-			<Projects />
-		</div>
-	);
+  return (
+    <div className="h-full min-h-screen w-full min-w-screen bg-bg text-text text-base font-urbanist p-4 sm:p-6 md:p-8 lg:p-10 space-y-4">
+      <Header />
+      <Technologies />
+      <Projects />
+      <Contacts />
+    </div>
+  );
 }
---
+++ src/components/Contacts.jsx
@@ -0,0 +1,30 @@
+import { SiGithub, SiMailgun } from "react-icons/si";
+import { SlSocialLinkedin } from "react-icons/sl";
+
+export default function Contacts() {
+	return (
+		<section className="max-w-4xl mx-auto space-y-4 py-10">
+			<h2 className="text-2xl font-semibold">Let's Connect</h2>
+			<p className="text-base">
+				Interested in working together or just want to chat about tech? Reach
+				out!
+			</p>
+			<div className="flex gap-3 items-center">
+				<span className="flex gap-2 items-center bg-surface-muted p-2 text-text-muted rounded-lg border border-border">
+					<SiMailgun size={14} />
+					<a href="mailto:adamskiptalam0@gmail.com">Email →</a>
+				</span>
+
+				<span className="flex gap-2 items-center bg-surface-muted p-2 text-text-muted rounded-lg border border-border">
+					<SiGithub size={14} />
+					<a href="https://github.com/kiptalam1">GitHub →</a>
+				</span>
+
+				<span className="flex gap-2 items-center bg-surface-muted p-2 text-text-muted rounded-lg border border-border">
+					<SlSocialLinkedin color="#0A66C2" size={14} />
+					<a href="https://linkedin.com/in/adams-kiptalam">LinkedIn →</a>
+				</span>
+			</div>
+		</section>
+	);
+}
